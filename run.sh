fc19d3ce1c26c17dd04ee3d5c0e0df0a424a723c
Marius <mariusdieckmann90@gmail.com>
2021-02-16 17:03:36 +0100

    explicit database name

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 wget -O Spike.sql $SQLDUMP_DL_LINK
-mysql --user $MYSQL_USERNAME --password $MYSQL_PASSWORD --host $SQL_HOST < Spike.sql
+mysql --user $MYSQL_USERNAME --password $MYSQL_PASSWORD --host $SQL_HOST Spike < Spike.sql
