e7bccccf2b647fe8bf69c1d51f110a25fa73304d
Marius <mariusdieckmann90@gmail.com>
2021-03-12 19:57:29 +0100

    using if exists syntax

--- run.sh
+++ run.sh
@@ -1,6 +1,4 @@
 wget -O Spike.sql $SQLDUMP_DL_LINK
 sed -i Spike.sql -e 's/utf8mb4_0900_ai_ci/utf8mb4_unicode_ci/g'
-set +e
-mysql --user $MYSQL_USERNAME --host $SQL_HOST -D Spike -e "DROP DATABASE Spike"
-set -e
+mysql --user $MYSQL_USERNAME --host $SQL_HOST -D Spike -e "DROP DATABASE IF EXISTS Spike"
 mysql --user $MYSQL_USERNAME --host $SQL_HOST Spike < Spike.sql
