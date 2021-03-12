f153304b30e1b83d6d312d8250130c740133b0fc
Marius <mariusdieckmann90@gmail.com>
2021-03-12 18:58:36 +0100

    added fix to handle non-existing database in drop

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 wget -O Spike.sql $SQLDUMP_DL_LINK
 sed -i Spike.sql -e 's/utf8mb4_0900_ai_ci/utf8mb4_unicode_ci/g'
-mysql --user $MYSQL_USERNAME --host $SQL_HOST -D Spike -e "DROP DATABASE Spike"
+mysql --user $MYSQL_USERNAME --host $SQL_HOST -D Spike -e "DROP DATABASE Spike" || true
 mysql --user $MYSQL_USERNAME --host $SQL_HOST Spike < Spike.sql
