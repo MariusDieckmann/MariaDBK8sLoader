e074d4be038ed71a4eaeac9e679aae3c7b50518c
Marius <mariusdieckmann90@gmail.com>
2021-03-12 20:30:12 +0100

    removed database reference in drop statement

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 wget -O Spike.sql $SQLDUMP_DL_LINK
 sed -i Spike.sql -e 's/utf8mb4_0900_ai_ci/utf8mb4_unicode_ci/g'
-mysql --user $MYSQL_USERNAME --host $SQL_HOST -D Spike -e "DROP DATABASE IF EXISTS Spike"
+mysql --user $MYSQL_USERNAME --host $SQL_HOST -e "DROP DATABASE IF EXISTS Spike"
 mysql --user $MYSQL_USERNAME --host $SQL_HOST -e "create database `Spike`"
 mysql --user $MYSQL_USERNAME --host $SQL_HOST Spike < Spike.sql
