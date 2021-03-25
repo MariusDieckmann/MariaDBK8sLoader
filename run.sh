c319ebb62ca3d75d95233f9a4567324a6f6283f3
Marius <mariusdieckmann90@gmail.com>
2021-03-25 15:19:37 +0100

    new update database structure

--- run.sh
+++ run.sh
@@ -1,5 +1,10 @@
-wget -O Spike.sql $SQLDUMP_DL_LINK
+wget -O Spike.sql $SPIKE_SQLDUMP_DL_LINK
+wget -O RKIPublic.sql $RKIPublic_SQLDUMP_DL_LINK
 sed -i Spike.sql -e 's/utf8mb4_0900_ai_ci/utf8mb4_unicode_ci/g'
+sed -i RKIPublic.sql -e 's/utf8mb4_0900_ai_ci/utf8mb4_unicode_ci/g'
 mysql --user $MYSQL_USERNAME --host $SQL_HOST -e "DROP DATABASE IF EXISTS Spike"
 mysql --user $MYSQL_USERNAME --host $SQL_HOST -e "create database Spike"
+mysql --user $MYSQL_USERNAME --host $SQL_HOST -e "DROP DATABASE IF EXISTS RKIPublic"
+mysql --user $MYSQL_USERNAME --host $SQL_HOST -e "create database RKIPublic"
 mysql --user $MYSQL_USERNAME --host $SQL_HOST Spike < Spike.sql
+mysql --user $MYSQL_USERNAME --host $SQL_HOST RKIPublic < RKIPublic.sql
