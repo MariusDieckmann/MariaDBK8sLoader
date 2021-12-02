9e07ccc7f77d68313963e51a7188509f9e27e3ff
Sebastian Beyvers <St4NNi@users.noreply.github.com>
2021-12-02 11:45:49 +0100

    Update run.sh
    Removed sleep,
    Added trap to get a proper stdout log.

--- run.sh
+++ run.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e
-sleep 10
+# TRAP last command to get a proper stdout/log entry for failed command.
+trap 'LAST_COMMAND=$CURRENT_COMMAND; CURRENT_COMMAND=$BASH_COMMAND' DEBUG
+trap 'ERROR_CODE=$?; FAILED_COMMAND=$LAST_COMMAND; echo "error: command \"$FAILED_COMMAND\" failed with exit code $ERROR_CODE"' ERR INT TERM
+
 wget -O Spike.sql -q -T 10 --tries 3 $SPIKE_SQLDUMP_DL_LINK
 wget -O RKIPublic.sql -q -T 10 --tries 3 $RKIPublic_SQLDUMP_DL_LINK
 sed -i Spike.sql -e 's/utf8mb4_0900_ai_ci/utf8mb4_unicode_ci/g'
